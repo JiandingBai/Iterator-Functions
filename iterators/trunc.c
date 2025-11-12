e206bb39927fc33a00bb78464e22268d9e11757d
Jianding <42047516+JiandingBai@users.noreply.github.com>
2025-11-12 11:55:49 -0800

    Add files via upload

---
+++ iterators/trunc.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "list.h"
+
+// Copy list.h and your completed list.c implementation into the q5 directory 
+// and use the provided Makefile to compile and test your implementation of trunc
+
+int main (int argc, char** argv) {
+  // TODO: your implementation of the trunc program
+}
